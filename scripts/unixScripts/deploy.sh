0209efabe42d8743d559298d010e6ad93dfb14a8
Jared Simpson <jaredsimpson@looselycanon.com>
2023-07-03 17:07:41 -0400

    feat: Tickets Created

--- scripts/unixScripts/deploy.sh
+++ scripts/unixScripts/deploy.sh
@@ -8,3 +8,6 @@ sfdx force:source:deploy -p "force-app/main/default" -l RunLocalTests -c
 sfdx force:source:deploy -p "force-app/main/default/reports"
 
 sfdx project:deploy:start -x manifest/Canon-Emails-V1.0.xml -l RunLocalTests
+
+sfdx project:deploy:start -x manifest/Prod-Align-7-3.xml
+sfdx project:deploy:start -x manifest/LCC-247.xml
